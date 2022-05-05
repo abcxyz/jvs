51168f58ca0e260a39a1e43ee4017ae9af366805
Ryan Serva <rsrv@google.com>
2022-05-05 17:45:18 +0000

    PR feedback

--- pkg/jvscrypto/rotation_handler_test.go
+++ pkg/jvscrypto/rotation_handler_test.go
@@ -248,6 +248,7 @@ func TestPerformActions(t *testing.T) {
 		log.Fatal(err)
 	}
 	clientOpt = option.WithGRPCConn(conn)
+	defer conn.Close()
 
 	c, err := kms.NewKeyManagementClient(ctx, clientOpt)
 	if err != nil {
