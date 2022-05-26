23eb0709ba6e5034743811e094a9f017695b2605
Ryan Serva <raserva@users.noreply.github.com>
2022-05-26 14:33:30 -0700

    feat: Updated server code to correctly set key id and add caching/updating for kms signer (#37)
    * feat: add config class and logic
    
    * PR feedback
    
    * change version from float to integer
    
    * changed int8 to uint 8 in config version
    
    * feat: Added protobufs and gen_protos script
    
    * added newline
    
    * removed extra newline
    
    * remove optional
    
    * s/v1/v0
    
    * adding implementation for jvs server
    
    * add newly generated protos
    
    * implementing jvs server
    
    * minor updates
    
    * adding config tests
    
    * PR feedback
    
    * Update pkg/config/justification_config.go
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    
    * feat: added signing code to jvs
    
    * minor updates
    
    * Update to not use gcp jwt library
    
    * minor cosmetic fixes
    
    * minor fixes
    
    * PR feedback
    
    * updated comment
    
    * ran go mod tidy and go format
    
    * PR feedback
    
    * fix issue
    
    * feat: Implementing cert rotation logic that leverages an external db to
    save some state
    
    * PR feedback
    
    * cont'd
    
    * PR feedback
    
    * updates
    
    * reduce nesting
    
    * added working implementation & tests
    
    * small fixes and comments
    
    * tidy
    
    * moar tests
    
    * changed to use labels
    
    * updated labels to match kms guidelines
    
    * remove unnecessary config members
    
    * added comment
    
    * fix bug where current time is not updated.
    
    * refactor to make state store an interface
    
    * add required dependency to main
    
    * added some comments
    
    * move method into state store
    
    * starting public key api
    
    * PR feedback
    
    * more public key implementation
    
    * minor updates
    
    * remove dependency, some PR feedback
    
    * add cache
    
    * only save primary in labels
    
    * change to en cache
    
    * fix one merge miss
    
    * clean up rotation handler
    
    * order similar functions together
    
    * go mod tidy
    
    * update to use zap
    
    * PR feedback
    
    * added the cache tests
    
    * fix comment
    
    * add return after http err
    
    * fix some linter issues
    
    * PR feedback
    
    * Update pkg/jvscrypto/key_hosting_test.go
    
    Co-authored-by: cshou <cshou@google.com>
    
    * adding client libs
    
    * added ECDSA key sanity check
    
    * update some other strings to remove escaped quotes
    
    * feat: adding client implementation
    
    * few small improvements
    
    * switched to JWX library
    
    * formatting
    
    * cache timeout validation
    
    * remove print statements
    
    * adding necessary changes to have create token api working
    
    * add doc
    
    * add validations and tests for config
    
    * update main class
    
    * PR feedback
    
    * updated to add context before signing
    
    * embedded signer
    
    * minor style update
    
    * update default
    
    * fix lint issues
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Co-authored-by: cshou <cshou@google.com>

--- pkg/config/justification_config_test.go
+++ pkg/config/justification_config_test.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"context"
 	"testing"
+	"time"
 
 	"github.com/abcxyz/jvs/pkg/testutil"
 	"github.com/google/go-cmp/cmp"
@@ -40,18 +41,24 @@ func TestLoadJustificationConfig(t *testing.T) {
 			cfg: `
 port: 123
 version: 1
+signer_cache_timeout: 1m
+issuer: jvs
 `,
 			wantConfig: &JustificationConfig{
-				Port:    "123",
-				Version: 1,
+				Port:               "123",
+				Version:            1,
+				SignerCacheTimeout: 1 * time.Minute,
+				Issuer:             "jvs",
 			},
 		},
 		{
 			name: "test_default",
 			cfg:  ``,
 			wantConfig: &JustificationConfig{
-				Port:    "8080",
-				Version: 1,
+				Port:               "8080",
+				Version:            1,
+				SignerCacheTimeout: 5 * time.Minute,
+				Issuer:             "jvs.abcxyz.dev",
 			},
 		},
 		{
@@ -63,18 +70,32 @@ version: 255
 			wantErr:    "failed validating config: 1 error occurred:\n\t* unexpected Version 255 want 1\n\n",
 		},
 		{
+			name: "test_invalid_signer_cache_timeout",
+			cfg: `
+signer_cache_timeout: -1m
+`,
+			wantConfig: nil,
+			wantErr:    "failed validating config: 1 error occurred:\n\t* cache timeout invalid: -60000000000\n\n",
+		},
+		{
 			name: "all_values_specified_env_override",
 			cfg: `
 version: 1
 port: 8080
+signer_cache_timeout: 1m
+issuer: jvs
 `,
 			envs: map[string]string{
-				"JVS_VERSION": "1",
-				"JVS_PORT":    "tcp",
+				"JVS_VERSION":              "1",
+				"JVS_PORT":                 "tcp",
+				"JVS_SIGNER_CACHE_TIMEOUT": "2m",
+				"JVS_ISSUER":               "other",
 			},
 			wantConfig: &JustificationConfig{
-				Version: 1,
-				Port:    "tcp",
+				Version:            1,
+				Port:               "tcp",
+				SignerCacheTimeout: 2 * time.Minute,
+				Issuer:             "other",
 			},
 		},
 	}
