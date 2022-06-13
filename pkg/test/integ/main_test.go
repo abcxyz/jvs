c15a688249c67eb1eba347bc6e7337826cf658f7
Ryan Serva <raserva@users.noreply.github.com>
2022-06-13 16:30:58 -0700

    Update pkg/test/integ/main_test.go
    Co-authored-by: cshou <cshou@google.com>
    
    Signed-off-by: Ryan Serva <raserva@users.noreply.github.com>

--- pkg/test/integ/main_test.go
+++ pkg/test/integ/main_test.go
@@ -121,7 +121,7 @@ func TestJVS(t *testing.T) {
 		"jti": nil,
 		"nbf": nil,
 	}
-	ignoreOpt := cmpopts.IgnoreMapEntries(func(k string, v interface{}) bool { _, ok := ignoreFields[k]; return ok })
+	ignoreOpt := cmpopts.IgnoreMapEntries(func(k string, _ interface{}) ok bool { _, ok = ignoreFields[k] })
 	if diff := cmp.Diff(expectedMap, tokenMap, ignoreOpt); diff != "" {
 		t.Errorf("Got diff (-want, +got): %v", diff)
 	}
