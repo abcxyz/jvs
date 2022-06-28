d0ce1f61462367921d9ff939c67684dec3b4a524
capri-xiyue <52932582+capri-xiyue@users.noreply.github.com>
2022-06-28 15:25:22 -0700

    refactor: decouple terraform with integration test (#73)
    * decouple terraform with integration test
    
    * added comments
    
    * fixed service account
    
    * add project id and keyring in the caller
    
    * added validation
    
    * hardcode project id
    
    * changed to env variable
    
    * added testjvs back
    
    * fixed nits
    
    * hardcode service account
    
    * use env variable for service account
    
    * fixed integration test

--- scripts/integration_build.sh
+++ scripts/integration_build.sh
@@ -12,25 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+set -u # -u makes bash error on undefined variables
 ROOT="$(cd "$(dirname "$0")/.." &>/dev/null; pwd -P)"
 
-JVS_SERVICE_ACCOUNT="jvs-service-sa@jvs-ci.iam.gserviceaccount.com"
-ROTATOR_SERVICE_ACCOUNT="rotator-sa@jvs-ci.iam.gserviceaccount.com"
-PUB_KEY_SERVICE_ACCOUNT="pubkey-sa@jvs-ci.iam.gserviceaccount.com"
-PROJECT_ID="jvs-ci"
+printf "Argument project_id is %s\n" "${PROJECT_ID}"
+printf "Argument keyring_id is %s\n" "${KEYRING_ID}"
 
-CI_DIR=${ROOT}/terraform/modules/ci-run
-
-cd $CI_DIR
-terraform init
-terraform apply -auto-approve \
-  -var="project_id=${PROJECT_ID}" \
-  -var="jvs_service_account=${JVS_SERVICE_ACCOUNT}" \
-  -var="rotator_service_account=${ROTATOR_SERVICE_ACCOUNT}" \
-  -var="public_key_service_account=${PUB_KEY_SERVICE_ACCOUNT}"
-
-export TEST_JVS_KMS_KEY_RING=$(terraform output key_ring)
+export TEST_JVS_KMS_KEY_RING="projects/${PROJECT_ID}/locations/global/keyRings/${KEYRING_ID}"
 export TEST_JVS_INTEGRATION=true
 
 cd ${ROOT}
