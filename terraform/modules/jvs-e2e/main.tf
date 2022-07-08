03160db1df46cbc8a1100da019f7b41d22b90eb2
Ryan Serva <raserva@users.noreply.github.com>
2022-07-08 10:08:30 -0700

    feat: Add necessary terraform setup for cert action api (#80)
    * added protos for new manual certificate action API
    
    * PR feedback
    
    * starting implementation of cert actions api
    
    * add necessary files for public sourcing
    
    * deleted other contribuing file
    
    * continuing implementation
    
    * more readmes
    
    * adding cert service to tf
    
    * finish cert service tf integration
    
    * PR feedback
    
    * Delete cert_action_server.go
    
    Signed-off-by: Ryan Serva <raserva@users.noreply.github.com>
    
    * Update main.go
    
    Signed-off-by: Ryan Serva <raserva@users.noreply.github.com>

--- terraform/modules/jvs-e2e/main.tf
+++ terraform/modules/jvs-e2e/main.tf
@@ -140,6 +140,19 @@ module "cert-rotator" {
   depends_on            = [google_kms_key_ring_iam_member.rotator_acc_roles]
 }
 
+module "cert-actions" {
+  source                = "../cert-action-service"
+  project_id            = var.project_id
+  key_id                = google_kms_crypto_key.asymmetric-sign-key.id
+  service_account       = google_service_account.rotator-acc.email
+  tag                   = local.tag
+  key_disabled_period   = var.key_disabled_period
+  key_grace_period      = var.key_grace_period
+  key_propagation_delay = var.key_propagation_delay
+  key_ttl               = var.key_ttl
+  depends_on            = [google_kms_key_ring_iam_member.rotator_acc_roles]
+}
+
 module "public-key" {
   source          = "../public-key"
   project_id      = var.project_id
