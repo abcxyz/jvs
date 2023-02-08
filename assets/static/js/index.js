bc756f5bd3a9ffbe26ffd67fbfabf96cfaac4a7b
Shan Kiyani <shankiyani@google.com>
2023-02-08 11:34:44 -0500

    turn off detect indentation, set to 2 spaces

--- assets/static/js/index.js
+++ assets/static/js/index.js
@@ -1,22 +1,22 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 document.addEventListener("DOMContentLoaded", function () {
-    // leverage the URL parameter provided from the request and set it to the target origin
-    const encodedUriComponent = urlParams.get("origin");
-    if (!encodedUriComponent) {
-        alert("An origin URL parameter must be provided.")
-        window.close();
-        return;
-    }
+  // leverage the URL parameter provided from the request and set it to the target origin
+  const encodedUriComponent = urlParams.get("origin");
+  if (!encodedUriComponent) {
+    alert("An origin URL parameter must be provided.")
+    window.close();
+    return;
+  }
 
-    const targetOrigin = decodeURIComponent(encodedUriComponent);
-    if (!targetOrigin) {
-        alert("Decoded URL parameter is invalid.")
-        window.close();
-        return;
-    }
+  const targetOrigin = decodeURIComponent(encodedUriComponent);
+  if (!targetOrigin) {
+    alert("Decoded URL parameter is invalid.")
+    window.close();
+    return;
+  }
 
-    // set values for the following hidden input elements, will be persisted to the next page
-    document.getElementById("origin").value = targetOrigin;
-    document.getElementById("windowname").value = window.name;
+  // set values for the following hidden input elements, will be persisted to the next page
+  document.getElementById("origin").value = targetOrigin;
+  document.getElementById("windowname").value = window.name;
 });
