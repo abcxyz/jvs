951d428205ed0d2ee6e78191759f6e604b8b763f
Shan Kiyani <shankiyani@google.com>
2023-02-21 12:29:29 -0800

    Shankiyani/initial popup (#181)
    * initial go template and styling
    
    * first pass at the server
    
    * send form labels to template
    
    * add reset button
    
    * remove print statement in server file, label todos
    
    * Update assets/templates/index.gohtml
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Signed-off-by: Shan Kiyani <shankiyani@google.com>
    
    * Update assets/templates/index.gohtml
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Signed-off-by: Shan Kiyani <shankiyani@google.com>
    
    * Update assets/templates/index.gohtml
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Signed-off-by: Shan Kiyani <shankiyani@google.com>
    
    * rename template file, format css file
    
    * set hidden field values via script tag
    
    * Add success page html, used for postMessage on page load
    
    * redo the template, dont use floats in css
    
    * run gofumpt and set timeouts for http server
    
    * Update pkg/ui/server.go
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Signed-off-by: Shan Kiyani <shan.a.raja@gmail.com>
    
    * Update pkg/ui/server.go
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Signed-off-by: Shan Kiyani <shan.a.raja@gmail.com>
    
    * clean up css
    
    * alert user when context is missing
    
    * add additional meta tags, format all files
    
    * turn off detect indentation, set to 2 spaces
    
    * Address comments, add config.go
    
    * add favicons
    
    * format reset.css and remove unused struct
    
    * add a forbidden page
    
    * Support allowlist env variable
    
    * Add some tests for validation methods
    
    * Update pkg/ui/server.go
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Signed-off-by: Shan Kiyani <shan.a.raja@gmail.com>
    
    * add new line to reset.css
    
    rename main script to success to match html file name
    rename windowname attribute to window-name
    move script tag inside body for both html files
    proper indentation on xml and json
    add copyright headers
    use querySelector over addEventListener
    use window for event listener
    shorthand err check on config
    make regex pattern a global variable
    
    * add testing for config file
    
    * add local ip check, remove helper handler, refactor tests
    
    * add _head.html.tmpl, rename main template file for popup
    
    * add Validate to cfg interface
    
    use logger instead of log
    in tests want and got or sufficient
    remove .flex-outer in css
    
    * refactor, inspired by EN service
    
    * resolve bugs from manual testing
    
    * run the linter
    
    * add basic testing for the popup handler
    
    * address comments, add a readme
    
    ---------
    
    Signed-off-by: Shan Kiyani <shankiyani@google.com>
    Signed-off-by: Shan Kiyani <shan.a.raja@gmail.com>
    Co-authored-by: Seth Vargo <seth@sethvargo.com>

--- go.mod
+++ go.mod
@@ -17,6 +17,8 @@ require (
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.14.0
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.37.0
+	go.uber.org/zap v1.24.0
+	golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb
 	golang.org/x/oauth2 v0.4.0
 	golang.org/x/sync v0.1.0
 	google.golang.org/api v0.106.0
@@ -60,7 +62,6 @@ require (
 	go.opentelemetry.io/otel/trace v1.11.2 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
---
+++ pkg/config/ui_config_test.go
@@ -0,0 +1,120 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"context"
+	"testing"
+
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+	"github.com/sethvargo/go-envconfig"
+)
+
+func TestNewConfig(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	cases := []struct {
+		name       string
+		envs       map[string]string
+		wantConfig *UIServiceConfig
+		wantErr    string
+	}{
+		{
+			name: "default_port",
+			envs: map[string]string{
+				"ALLOWLIST": "example.com",
+			},
+			wantConfig: &UIServiceConfig{
+				Port:      "9091",
+				Allowlist: []string{"example.com"},
+				DevMode:   false,
+			},
+		},
+		{
+			name: "override_port",
+			envs: map[string]string{
+				"PORT":      "1010",
+				"ALLOWLIST": "example.com",
+			},
+			wantConfig: &UIServiceConfig{
+				Port:      "1010",
+				Allowlist: []string{"example.com"},
+			},
+		},
+		{
+			name: "dev_mode_on",
+			envs: map[string]string{
+				"DEV_MODE":  "true",
+				"ALLOWLIST": "example.com",
+			},
+			wantConfig: &UIServiceConfig{
+				Port:      "9091",
+				DevMode:   true,
+				Allowlist: []string{"example.com"},
+			},
+		},
+		{
+			name:       "no_port_no_allowlist",
+			envs:       map[string]string{},
+			wantConfig: nil,
+			wantErr:    "failed to parse server config:",
+		},
+		{
+			name: "asterisks_in_allowlist",
+			envs: map[string]string{
+				"ALLOWLIST": "example.com;*",
+			},
+			wantConfig: nil,
+			wantErr:    "asterisk(*) must be exclusive, no other domains allowed",
+		},
+		{
+			name: "exclusive_asterisk",
+			envs: map[string]string{
+				"ALLOWLIST": "*",
+			},
+			wantConfig: &UIServiceConfig{
+				Port:      "9091",
+				Allowlist: []string{"*"},
+			},
+		},
+		{
+			name: "multiple_domains",
+			envs: map[string]string{
+				"ALLOWLIST": "subdomain.foo.com;*.example.com",
+			},
+			wantConfig: &UIServiceConfig{
+				Port:      "9091",
+				Allowlist: []string{"subdomain.foo.com", "*.example.com"},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			lookuper := envconfig.MapLookuper(tc.envs)
+			gotConfig, err := newUIConfig(ctx, lookuper)
+			if diff := testutil.DiffErrString(err, tc.wantErr); diff != "" {
+				t.Errorf("Unexpected err: %s", diff)
+			}
+			if diff := cmp.Diff(tc.wantConfig, gotConfig); diff != "" {
+				t.Errorf("Config unexpected diff (-want,+got):\n%s", diff)
+			}
+		})
+	}
+}
