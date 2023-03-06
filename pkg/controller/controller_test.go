3d7a983e1f3fd2b2d365ad59d680c5756772f354
Shan Kiyani <shankiyani@google.com>
2023-03-06 16:36:23 -0500

    another test

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -162,13 +162,19 @@ func TestValidateOrigin(t *testing.T) {
 			wantRes:   true,
 		},
 		{
-			name:      "local_origin",
+			name:      "localhost_origin",
+			origin:    "http://localhost",
+			allowlist: []string{"example.com"},
+			wantRes:   true,
+		},
+		{
+			name:      "local_ip_origin",
 			origin:    "http://127.0.0.1",
 			allowlist: []string{"example.com"},
 			wantRes:   true,
 		},
 		{
-			name:      "private_origin",
+			name:      "private_ip_origin",
 			origin:    "http://10.0.0.1",
 			allowlist: []string{"example.com"},
 			wantRes:   true,
