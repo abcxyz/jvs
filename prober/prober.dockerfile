54c2419644d21fc9555dd5662332e00bdffb06e3
sailorlqh <qinhang@google.com>
2023-04-07 18:08:25 +0000

    remove breakglass test

--- prober/prober.dockerfile
+++ prober/prober.dockerfile
@@ -2,7 +2,7 @@
 FROM cgr.dev/chainguard/bash:latest
 
 COPY jvsctl /jvsctl
-COPY ./prober/prober.sh prober.sh
+COPY ./prober/prober.sh /prober.sh
 
 # Normally we would set this to run as "nobody".
 # But goreleaser builds the binary locally and sometimes it will mess up the permission
@@ -12,4 +12,4 @@ COPY ./prober/prober.sh prober.sh
 
 # Run the web service on container startup.
 
-ENTRYPOINT ["bash", "prober.sh"]
+ENTRYPOINT ["/prober.sh"]
--- prober/prober.sh
+++ prober/prober.sh
@@ -1,11 +1,7 @@
 set -eEuo pipefail
 
-ID_TOKEN=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=$AUDIENCE" -H "Metadata-Flavor: Google")
+ID_TOKEN=$(curl -sF "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=$AUDIENCE" -H "Metadata-Flavor: Google")
 
-./jvsctl token --auth-token $ID_TOKEN -e "prober" > jvs_token
+./jvsctl token --auth-token $ID_TOKEN -e "jvs_prober" > jvs_token
 
 cat jvs_token | ./jvsctl validate --token -
-
-./jvsctl token --auth-token $ID_TOKEN -e "prober" > jvs_breakglass_token
-
-cat jvs_breakglass_token | ./jvsctl validate --token -
