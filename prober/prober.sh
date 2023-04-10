323fc3c70253d5b4f9b8be5af9417db82737cff9
Qinhang Li <55097418+sailorlqh@users.noreply.github.com>
2023-04-10 09:27:21 -0700

    Feat: add prober script and dockerfiles, update goreleaser.docker.yaml to build prober image (#240)
    * add prober script and dockerfiles, update goreleaser.docker.yaml
    
    * move prober folder to repo root
    
    * update build_flag_templates labels for prober service
    
    * add prober arm64 build and fix nits
    
    * remove breakglass test
    
    * fix docker permission
    
    ---------
    
    Signed-off-by: Qinhang Li <55097418+sailorlqh@users.noreply.github.com>

---
+++ prober/prober.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eEuo pipefail
+
+ID_TOKEN=$(curl -sf "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=$AUDIENCE" -H "Metadata-Flavor: Google")
+
+./jvsctl token --auth-token $ID_TOKEN -e "jvs_prober" > jvs_token
+
+cat jvs_token | ./jvsctl validate --token -
