70bc6fd2d3310fd28c7fb263c7e788f9e3f478cb
Dandan Shen <dandans@google.com>
2023-07-26 23:52:51 +0000

    add terraform to take in jira plugin variables

--- terraform/modules/jvs-services/variables.tf
+++ terraform/modules/jvs-services/variables.tf
@@ -109,3 +109,16 @@ variable "ui_envvars" {
     "JVS_UI_ALLOWLIST" = "*",
   }
 }
+
+variable "jira_plugin_envvars" {
+  description = "Env vars for Jira plugin."
+  type        = map(string)
+  default = {
+    "JIRA_PLUGIN_ENDPOINT" : "https://blahblah.atlassian.net/rest/api/3",
+    "JIRA_PLUGIN_JQL" : "project = JRA and assignee != jsmith",
+    "JIRA_PLUGIN_ACCOUNT" : "abc@xyz.com",
+    "JIRA_PLUGIN_API_TOKEN_SECRET_ID" : "projects/123456/secrets/api-token/versions/4",
+    "JIRA_PLUGIN_DISPLAY_NAME" : "Jira Issue Key",
+    "JIRA_PLUGIN_HINT" : "Jira Issue Key under JVS project",
+  }
+}
