6b9bb9862b1ff558a28f9fc713fccca9a5bd29de
XiyueYu <52932582+capri-xiyue@users.noreply.github.com>
2023-09-21 16:41:32 -0700

    use release tag

--- templates/jvs-e2e/jvs-e2e.tf
+++ templates/jvs-e2e/jvs-e2e.tf
@@ -17,7 +17,7 @@ locals {
 }
 
 module "jvs_e2e" {
-  source = "git::https://github.com/abcxyz/jvs.git//terraform/e2e?ref=REPLACE_JVS_SHA"
+  source = "git::https://github.com/abcxyz/jvs.git//terraform/e2e?ref=vREPLACE_JVS_RELEASE_VERSION"
 
   project_id = local.project_id
 
@@ -25,7 +25,7 @@ module "jvs_e2e" {
 
   kms_key_location    = "REPLACE_KMS_KEY_LOCATION"
   jvs_invoker_members = REPLACE_JVS_INVOKE_MEMBERS
-  jvs_container_image = "REPLACE_JVS_CONTAINER_IMAGE"
+  jvs_container_image = "us-docker.pkg.dev/abcxyz-artifacts/docker-images/jvsctl:REPLACE_JVS_RELEASE_VERSION-amd64"
   jvs_api_domain      = "REPLACE_JVS_API_DOMAIN"
   jvs_ui_domain       = "REPLACE_JVS_UI_DOMAIN"
   iap_support_email   = "REPLACE_IAP_SUPPORT_EMAIL"
@@ -34,7 +34,7 @@ module "jvs_e2e" {
 
   // Use gcloud app id because Cloud Run accepts it.
   prober_audience  = "REPLACE_PROBER_AUDIENCE"
-  jvs_prober_image = "REPLACE_JVS_PROBER_IMAGE"
+  jvs_prober_image = "us-docker.pkg.dev/abcxyz-artifacts/docker-images/jvs-prober:REPLACE_JVS_RELEASE_VERSION-amd64"
   alert_enabled    = REPLACE_ALERT_ENABLED
 
   plugin_envvars = REPLACE_PLUGIN_ENVVARS
