3175a5c3221328e61b35167d206408f3fc2c41ea
XiyueYu <52932582+capri-xiyue@users.noreply.github.com>
2023-10-05 12:51:46 -0700

    revert cloud run update

--- terraform/modules/jvs-services/jvs-api.tf
+++ terraform/modules/jvs-services/jvs-api.tf
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 module "api_cloud_run" {
-  source = "git::https://github.com/abcxyz/terraform-modules.git//modules/cloud_run?ref=580de3cc99dce5f37f7698b14e04fdd1c86ab15a"
+  source = "git::https://github.com/abcxyz/terraform-modules.git//modules/cloud_run?ref=46d3ffd82d7c3080bc5ec2cc788fe3e21176a8be"
 
   project_id = var.project_id
 
