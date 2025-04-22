f5d7f63be21bbcbc54240ed1386c1248e30a9786
Dylan Yurgionas <dcreey@google.com>
2025-04-22 16:30:19 +0000

    fix: lint

--- prober/prober.sh
+++ prober/prober.sh
@@ -9,5 +9,5 @@ ID_TOKEN=$(curl -sf "http://metadata.google.internal/computeMetadata/v1/instance
 
 ./jvsctl token create --auth-token "${ID_TOKEN}" -e "jvs_prober" > jvs_token
 
-# shellint disable=SC2002
+# shellcheck disable=SC2002
 cat jvs_token | ./jvsctl token validate --token -
