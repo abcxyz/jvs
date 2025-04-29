f5f2ba746c20d3e2022945c6e97bd0ad4f156473
Dylan Yurgionas <dcreey@gmail.com>
2025-04-29 15:24:22 +0000

    fix: lint issues (#430)
    Temporarily adds the lint.yml from abcxyz/actions, then fixes lint
    problems, then removes the lint.yml. This repo will be added to the org
    ruleset so it gets linted after this merges.
    
    Tested expected java format lint fail:
    https://github.com/abcxyz/jvs/actions/runs/14621743965/job/41023298247?pr=430
    
    ---------
    
    Signed-off-by: Dylan Yurgionas <dcreey@gmail.com>

--- prober/prober.sh
+++ prober/prober.sh
@@ -2,8 +2,12 @@
 
 set -eEuo pipefail
 
-ID_TOKEN=$(curl -sf "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=$AUDIENCE" -H "Metadata-Flavor: Google")
+# Expected to be set externally.
+declare -r AUDIENCE
 
-./jvsctl token create --auth-token $ID_TOKEN -e "jvs_prober" > jvs_token
+ID_TOKEN=$(curl -sf "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${AUDIENCE}" -H "Metadata-Flavor: Google")
 
+./jvsctl token create --auth-token "${ID_TOKEN}" -e "jvs_prober" > jvs_token
+
+# shellcheck disable=SC2002
 cat jvs_token | ./jvsctl token validate --token -
